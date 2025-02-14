ce430194ae50b95d14146a97100db3f4a5f12dbb
barsatKhadka <khadkabarsat598@gmail.com>
2025-02-14 16:31:48 -0600

    add: "SELECT" query items are stored in hashset for use.

--- JsonSQL/src/main/java/org/example/core/QueryExecutor.java
+++ JsonSQL/src/main/java/org/example/core/QueryExecutor.java
@@ -2,16 +2,43 @@ package org.example.core;
 
 import org.example.Model.ParsedQueryFields;
 
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
 public class QueryExecutor {
+    Set<Object> selectFields = new HashSet();
     public void executeQuery(ParsedQueryFields parsedQueryFields) {
         String select = parsedQueryFields.getSelect();
         String from = parsedQueryFields.getFrom();
         String where = parsedQueryFields.getWhere();
         String alias  = parsedQueryFields.getAlias();
 
-        System.out.println(select);
-        System.out.println(from);
-        System.out.println(where);
-        System.out.println(alias);
+        if(select.isEmpty()  || from.isEmpty()){
+            throw new IllegalArgumentException("select or from statement missing in JsonSQL.");
+        }
+
+        if(select.contains(",")){
+            String[] fields = select.split(",");
+            for(String field : fields){
+                String trimmedField = field.trim();
+                Object selectItem = parseField(trimmedField);
+                selectFields.add(selectItem);
+            }
+        }
+        else{
+            Object selectItem = parseField(select);
+            selectFields.add(selectItem);
+        }
+
+        System.out.println(selectFields);
+
+    }
+    private Object parseField(String field) {
+        try {
+            return Double.parseDouble(field);
+        } catch (NumberFormatException e) {
+            return field;
+        }
     }
 }
